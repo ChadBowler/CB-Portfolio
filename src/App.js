b9a5a1851f4f797dff6ff2eb241f97c0ed036749
Chad Bowler <chadbowler@gmail.com>
2023-08-09 22:46:14 -0600

    removed an import causing the heroku build to fail

--- src/App.js
+++ src/App.js
@@ -7,7 +7,6 @@ import Contact from './Contact';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-import './App.css';
 
 function App() {
   return (
